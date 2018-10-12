6bc83efd84f6128ed7c7c4fd62d2a43b6e4314ce
brennanclark <brennanpclark@gmail.com>
2018-10-12 20:00:13 +0000

    The tweet-text field now empties anytime a tweet is posted

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,33 +1,58 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 $(document).ready(function() {
-
+//--------------------------Utility Functions----------------------------------
+//converts the unix time to the elapsed time between the present
+//and the time the post was made
   function formatDate(unixTime) {
     return moment(unixTime).fromNow();
-  }
-
+  };
+//Factry function that accepts an array of classnames and adds them to the <i> tag
   function iconFactory(className) {
     return $('<i></i>').addClass(className);
-  }
-
+  };
+//--------------------------------------------------------------------------
+//-------------------------Render Tweets------------------------------------
+//Loops through an array of objects and prepends each one to the previos one.
+  function renderTweets(tweets) {
+    $('.tweet-container').empty();
+    tweets.forEach(function (tweet){
+      createTweetElement(tweet).prependTo('.tweet-container');
+    });
+  };
+//--------------------------Post Tweet---------------------------------------
+//Uses ajax to make a Post request to /tweets  with the serialized return from
+//createTweetElement and if it is successful loads the tweets. On error it returns
+//an error.
   function postTweet (data){
     $.ajax({
-          method: 'POST',
-          data: data,
-          url: '/tweets',
-          success: function(result){
-            loadTweets();
-          },
-          error: function(err){
-            console.log('error');
-          }
-        })
-  }
-
-
+      method: 'POST',
+      data: data,
+      url: '/tweets',
+      success: function(result){
+        loadTweets();
+      },
+      error: function(err){
+        console.log('error');
+      }
+    });
+  };
+//---------------------------Load tweets-----------------------------------
+//Uses ajax to make a get request from the /tweets path and either returns the error on error
+//or calls the render tweets function and passes it the tweet objects(result) on success.
+  function loadTweets(){
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+      success: function(result){
+        renderTweets(result);
+      },
+      error: function(err){
+        console.log('error');
+      }
+    });
+  };
+//---------------------------Create Tweet element--------------------------
+//Takes an object as input and assigns its values to the appropriate HTML tags and values
+//assembles all the elements together and the returns the newly created element.
   function createTweetElement({ user: { avatars, name, handle }, content, created_at }) {
     const $tweet         = $("<article class='tweet'>");
     const $header        = $('<header></header>').appendTo($tweet);
@@ -44,59 +69,44 @@ $(document).ready(function() {
     const $socialButtons = $('<div class="social-buttons"></div>').append(icons).appendTo($footer);
 
     return $tweet;
-  }
+  };
+//--------------------------SUBMIT TWEET---------------------------------
+//Takes the input text from the tweet field, validates it and either gives the appropriate
+//error resoponse or posts the tweet and then clears the field and resets the
+//character counter.
+  $('#tweetform').on('submit', function () {
+    event.preventDefault();
+    let data = $('#tweetform').serialize();
+    let userText = data.replace("text=","");
+    let $error = $('.error');
 
-  function renderTweets(tweets) {
-    $('.tweet-container').empty();
-    tweets.forEach(function (tweet){
-      createTweetElement(tweet).prependTo('.tweet-container');
-    });
-  }
-
-  function loadTweets(){
-    $.ajax({
-      method: 'GET',
-      url: '/tweets',
-      success: function(result){
-        renderTweets(result);
-      },
-      error: function(err){
-        console.log('error');
+    if (userText.length === 0) {
+      if($error.text() !== 'Tweet too short'){
+        $error.slideUp(function(){
+          $error.text('Tweet too short').slideDown();
+        });
       }
-    })
-  }
+      return;
+    }
+    if (userText.length > 140 ) {
+      if ($error.text() !== 'Tweet too long') {
+        $error.slideUp(function(){
+          $error.text('Tweet too long').slideDown();
+        });
 
-    $('#tweetform').on('submit', function () {
-      event.preventDefault();
-      let data = $('#tweetform').serialize();
-      let userText = data.replace("text=","");
-      let $error = $('.error')
-
-      if (userText.length === 0) {
-        if($error.text() !== 'Tweet too short'){
-          $error.slideUp(function(){
-            $error.text('Tweet too short').slideDown();
-          })
-        }
-        return;
       }
-      if (userText.length > 140 ) {
-        if ($error.text() !== 'Tweet too long') {
-          $error.slideUp(function(){
-            $error.text('Tweet too long').slideDown();
-          });
-
-        }
-
-        return;
-      }
-
-      $error.slideUp();
-      postTweet(data);
-    });
-
-  loadTweets();
-
+      return;
+    }
+    $error.slideUp();
+    postTweet(data);
+    $('#tweetText').val('');
+    $('.counter').text(140);
+  });
+//--------------------------COMPOSE BUTTON-------------------------------
+// When the compose button is clicked the current position of the page is validated and
+//it either scrolls up to the input field and focuses on it or it scrolls down to the first
+//tweet in the timeline. I am aware that the functionality was supposed to hide
+//the tweet field but I prefer this approach for aesthetic reasons.
   $('.compose').on('click', function() {
     let body = $('html, body');
     let tweetContainer = $('.tweet-container').offset().top;
@@ -109,7 +119,9 @@ $(document).ready(function() {
         $('#tweetText').focus();
       })
     }
-  })
+  });
+//Calls the loadTweets function.
+  loadTweets();
 });
 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-
+//Updates the Chracater counter everytime a new character is input into the text field
+//has checks to set a threshhold for when the numbers should turn red.
   $('.new-tweet textArea').on('input', function(event){
 
     let charMax = 140;
